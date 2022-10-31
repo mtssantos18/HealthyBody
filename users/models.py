4ac090ceb3bb69ac9df8144654f25c52f37b371f
Matheus Santos <eumtssantos@outlook.com>
2022-10-31 15:51:39 -0300

    feat: initial organization

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from email.policy import default
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    email = models.CharField(max_length=127, unique=True)
+    username = models.CharField(max_length=25, unique=True)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def obtain_full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
