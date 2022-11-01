91afbd014d29a03b19602f04125ad94972702327
Matheus Santos <eumtssantos@outlook.com>
2022-11-01 16:40:46 -0300

    feat: still doing my part

--- modalities/views.py
+++ modalities/views.py
@@ -1,5 +1,21 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+
+from modalities.models import Modality
+from modalities.permissions import IsAdmPermission
+from modalities.serializers import ModalitySerializer
 
 
 class ModalityView(generics.ListCreateAPIView):
-    ...
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdmPermission]
+
+    queryset = Modality.objects.all()
+    serializer_class = ModalitySerializer
+
+
+class ModalityDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+
+    queryset = Modality.objects.all()
+    serializer_class = ModalitySerializer
