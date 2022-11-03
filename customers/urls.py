1b02d7040c909410adb7c89efb3c05a75a70b3e5
chrds1 <henriquerodriguez0@gmail.com>
2022-11-03 15:07:14 -0300

    feat: updated customer

--- customers/urls.py
+++ customers/urls.py
@@ -4,4 +4,5 @@ from customers import views
 
 urlpatterns = [
     path("customers/", views.CreateListCustomerView.as_view()),
+    path("customers/<pk>/", views.RetrieveUpdateDeleteCustomerView.as_view()),
 ]
