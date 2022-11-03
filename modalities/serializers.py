10b632e86f02a9f481694df1f100fc0e2dd31352
Matheus Santos <eumtssantos@outlook.com>
2022-11-03 19:22:12 -0300

    feat: creating class detail view

--- modalities/serializers.py
+++ modalities/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
+
 from modalities.models import Modality
-from teachers.serializer import TeachersSerializer
+from teachers.models import Teacher
+
+from users.serializers import GeneralUserSerializer
+
+
+class TeachersSerializer(serializers.ModelSerializer):
+
+    user = GeneralUserSerializer()
+
+    class Meta:
+
+        model = Teacher
+
+        fields = "__all__"
 
 
 class ModalitySerializer(serializers.ModelSerializer):
