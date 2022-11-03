815d83626315036932150917074927af05586350
Matheus Santos <eumtssantos@outlook.com>
2022-11-03 20:00:41 -0300

    feat: ClassesDetailView done

--- customers/models.py
+++ customers/models.py
@@ -3,7 +3,11 @@ import uuid
 
 
 class Customer(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
 
     user = models.OneToOneField("users.User", on_delete=models.CASCADE)
-    plan = models.ForeignKey("plans.Plan", on_delete=models.CASCADE, related_name="users")
+    plan = models.ForeignKey(
+        "plans.Plan", on_delete=models.CASCADE, related_name="users"
+    )
