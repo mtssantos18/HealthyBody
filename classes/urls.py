ddebebe7efb384efb31b4d330c2f2f55e6a49614
Matheus Santos <eumtssantos@outlook.com>
2022-11-07 18:14:49 -0300

    feat: Classes improves views, now customer can join and leave classes, and it's possible to list classes with vacancies

--- classes/urls.py
+++ classes/urls.py
@@ -3,5 +3,8 @@ from classes import views
 
 urlpatterns = [
     path("class/", views.ClassView.as_view()),
+    path("class/open/", views.ListOpenClassView.as_view()),
     path("class/<class_id>/", views.ClassDetailView.as_view()),
+    path("class/join/<class_id>/", views.ClassAddCustomerView.as_view()),
+    path("class/remove/<class_id>/", views.ClassRemoveCustomerView.as_view()),
 ]
