fde62de1eafae006ac71c4d6e79a4cb934715ff1
Matheus Santos <eumtssantos@outlook.com>
2022-11-07 10:15:44 -0300

    fix: identing code

--- private_trainings/urls.py
+++ private_trainings/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("customer/private/auth/",views.PrivateViewCustomer.as_view()),
-    path("private_training/",views.GetAllPrivateTrainings.as_view()),
-    path("personal/private/auth/",views.PrivateViewPersonal.as_view()),
-    path("private_training/personal/<uuid:training_id>",views.PrivateTrainingDetailForPersonals.as_view()),
-    path("private_training/customer/<uuid:training_id>",views.PrivateTrainingDetailForCustomers.as_view()),
+    path("customer/private/auth/", views.PrivateViewCustomer.as_view()),
+    path("private_training/", views.GetAllPrivateTrainings.as_view()),
+    path("personal/private/auth/", views.PrivateViewPersonal.as_view()),
+    path(
+        "private_training/personal/<uuid:training_id>",
+        views.PrivateTrainingDetailForPersonals.as_view(),
+    ),
+    path(
+        "private_training/customer/<uuid:training_id>",
+        views.PrivateTrainingDetailForCustomers.as_view(),
+    ),
 ]
