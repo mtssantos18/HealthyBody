93d3e302dac3c8cbf27dfebeef38936b04b8f2c5
chrds1 <henriquerodriguez0@gmail.com>
2022-11-07 21:10:18 -0300

    feat create emails

--- _healthy_body/settings.py
+++ _healthy_body/settings.py
@@ -182,3 +182,10 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVICE_INCLUDE_SCHEMA": False,
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
